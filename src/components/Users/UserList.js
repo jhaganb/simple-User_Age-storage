baca77dbf5022b652662e9064066946bbc342dd4
jhaganb <jhaganb@gmail.com>
2023-02-25 23:20:36 +0530

    made protal and useRef

--- src/components/Users/UserList.js
+++ src/components/Users/UserList.js
@@ -9,7 +9,7 @@ const UserList = (props) => {
       <ul>
         {props.users.map((user) => (
           <li key={user.id}>
-            {user.name} ({user.age} years old)
+            {user.name} ({user.age} years old.)
           </li>
         ))}
       </ul>
